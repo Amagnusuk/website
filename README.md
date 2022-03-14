1eeb77be71eccd4841dad14ec127fc34d55dc9bd
Amagnusuk <95133995+Amagnusuk@users.noreply.github.com>
2022-03-14 18:01:14 +0000

    Fiz a área de Services
    Dei continuidade nos Cards e fiz a área de Services.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
  Trabalhando css e html
 
  Trabalhei nos Cards e o seu css
+
+ Hoje dei continuidade nos Cards e início na área de Services
--- includes/home.php
+++ includes/home.php
@@ -1,31 +1,32 @@
 <main>
-    <!------ INTRO SECTION ------>
+    <!------INTRO SECTION------>
+    <!------INTRO SECTION------>
     <section id="intro">
         <div class="container">
             <div class="intro-text line">
                 <h1>We´re a digital agency.</h1>
                 <a href="#" class="btn btn-dark">Getting Started</a>
             </div>
-            <!------ CARDS ------>
+            <!------ CARDS ------> 
             <!------ CARDS ------>
             <div class="intro-cards">
                 <div class="card">
                     <i class="fas fa-chart-bar"></i>
-                    <h3 class="heading-3">Future Vision</h3>
+                    <h3 class="line heading-3">Future Vision</h3>
                     <p>
                         it is a long established fact that a reader will be distracted by the readable content of a page.
                     </p>
                 </div>
                 <div class="card">
                     <i class="fas fa-rocket"></i>
-                    <h3 class="heading-3">Product Design</h3>
+                    <h3 class="line heading-3">Product Design</h3>
                     <p>
                         There are many variations of the passages of available, but the majority alterarion in some form.
                     </p>
                 </div>
                 <div class="card">
                     <i class="fas fa-chart-pie"></i>
-                    <h3 class="heading-3">Great Solution</h3>
+                    <h3 class="line heading-3">Great Solution</h3>
                     <p>
                         The generated Lorem Ipsum is therefore always free from repitiotion, injected humor.
                     </p>
@@ -35,4 +36,54 @@
             <!--xx-- CARDS --xx-->
         </div>
     </section>
+    <!--xx--INTRO SECTION--xx-->
+    <!--xx--INTRO SECTION--xx-->
+
+    <!------SERVICES SECTION------>
+    <!------SERVICES SECTION------>
+    <section id="services">
+        <div class="container">
+            <div class="heading line">
+                <h2>services</h2>
+                <p>WE WORK WITH YOU, NOT FOR YOU</p>
+            </div>
+            <!------ SERVICES------>
+            <div class="services">
+                <div class="service">
+                    <i class="fas fa-bookmark"></i>
+                    <h3>Digital Strategy</h3>
+                    <i class="fas fa-arrow-right"></i>
+                </div>
+                <div class="service">
+                    <i class="fas fa-file-alt"></i>
+                    <h3>UX Design</h3>
+                    <i class="fas fa-arrow-right"></i>
+                </div>
+                <div class="service">
+                    <i class="fas fa-edit"></i>
+                    <h3>Product Design</h3>
+                    <i class="fas fa-arrow-right"></i>
+                </div>
+                <div class="service">
+                    <i class="fas fa-bookmark"></i>
+                    <h3>content Strategy</h3>
+                    <i class="fas fa-arrow-right"></i>
+                </div>
+                <div class="service">
+                    <i class="fas fa-file-alt"></i>
+                    <h3>Brand Identity</h3>
+                    <i class="fas fa-arrow-right"></i>
+                </div>
+                <div class="service">
+                    <i class="fas fa-edit"></i>
+                    <h3>Media Planning</h3>
+                    <i class="fas fa-arrow-right"></i>
+                </div>
+            </div>
+            <!--xx-- SERVICES--xx-->
+        </div>
+    </section>
+    <!--xx--SERVICES SECTION--xx-->
+    <!--xx--SERVICES SECTION--xx-->
+
 </main>
