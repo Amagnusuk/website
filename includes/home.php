a1584a4ee0f1c289d62468d1396d26748ea94b93
Amagnusuk <95133995+Amagnusuk@users.noreply.github.com>
2022-03-15 12:24:45 +0000

    Trabalhei o Blog e Services Section
    Mais um trabalho na área de Section

--- includes/home.php
+++ includes/home.php
@@ -40,7 +40,7 @@
     <!--xx--INTRO SECTION--xx-->
 
     <!------SERVICES SECTION------>
-    <!------SERVICES SECTION------>
+    <!------SERVICES SECTION------> 
     <section id="services">
         <div class="container">
             <div class="heading line">
@@ -83,7 +83,106 @@
             <!--xx-- SERVICES--xx-->
         </div>
     </section>
+    <!------VERTICAL LINE------>
+    <div class="h-line"></div>
+    <!--xx--VERTICAL LINE--xx-->
     <!--xx--SERVICES SECTION--xx-->
     <!--xx--SERVICES SECTION--xx-->
+
+    <!------ABOUT SECTION------>
+    <!------ABOUT SECTION------>
+    <section id="about">
+        <div class="container">
+            <div class="heading line">
+                <h2>About</h2>
+                <p>WE´RE MORE THAN A DIGITAL AGENCY</p>
+            </div>
+            <!------IMAGE GRID------>
+            <div class="about">
+                <div class="image image1">
+                    <img src="assets/images/about_01.jpg" alt="">
+                    <div class="image-text">
+                        <h3 class="heading-3">Teamwork</h3>
+                        <p>Comited and Creative</p>
+                    </div>
+                </div>
+
+                <!------IMAGE 2------>
+                <div class="image image2">
+                    <img src="assets/images/about_02.jpg" alt="">
+                    <div class="image-text">
+                        <h3 class="heading-3">Philosophy</h3>
+                        <p>Trust pays off</p>
+                    </div>
+                </div>
+
+                <!------IMAGE 3------>
+                <div class="image image3">
+                    <img src="assets/images/about_03.jpg" alt="">
+                    <div class="image-text">
+                        <h3 class="heading-3">Office</h3>
+                        <p>Ikeja, Lagos</p>
+                    </div>
+                </div>
+            </div>
+
+            <!------ABOUT TEXT------>
+            <div class="about-text">
+                <div class="text1">
+                    <h3 class="heading-3">Who we are</h3>
+                    <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward will have multiple touchpoints for offshoring.</p>
+                </div>
+                <div class="text2">
+                    <h3 class="heading-3">Our philosophy</h3>
+                    <p>A new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time.</p>
+                </div>
+                <div class="text3">
+                    <h3 class="heading-3">How we work</h3>
+                    <p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!------BLOG SECTION------>
+    <section id="blog">
+        <div class="container">
+            <div class="heading">
+                <h2>Latest</h2>
+                <p>WE´RE MORE THAN A DIGITAL AGENCY</p>
+            </div>
+
+            <!------BLOG ARTICLES------>
+            <div class="blog about-text">
+                <div class="blog-item image">
+                    <img src="assets/images/01-min.jpg" alt="">
+                    <div class="blog-text">
+                        <p>DESIGN/UX</p>
+                        <h3>How to drive your customer experience</h3>
+                        <i class="fas fa-arrow-right"></i>
+                    </div>
+                </div>
+
+                <!------BLOG 2------>
+                <div class="blog-item image">
+                    <img src="assets/images/02-min.jpg" alt="">
+                    <div class="blog-text">
+                        <p>DESIGN/UX</p>
+                        <h3>How to drive your customer experience</h3>
+                        <i class="fas fa-arrow-right"></i>
+                    </div>
+                </div>
+
+                <!------BLOG 3------>
+                <div class="blog-item image">
+                    <img src="assets/images/03-min.jpg" alt="">
+                    <div class="blog-text">
+                        <p>DESIGN/UX</p>
+                        <h3>How to drive your customer experience</h3>
+                        <i class="fas fa-arrow-right"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 </main>
